0ecb5e6c34698dfc6e9515c7e27f4d86b4c7dcb6
Anjali Silva <anjalisilva99@gmail.com>
2026-07-21 18:48:16 -0500

    add function for load and check

--- code/aerodyne_case_study.R
+++ code/aerodyne_case_study.R
@@ -63,8 +63,8 @@ save_figures <- ask_yes_no(
 )
 
 cat("Save figures:",
-  if (save_figures) "Yes" else "No",
-  "\n")
+    if (save_figures) "Yes" else "No",
+    "\n")
 # assumptions ----------------------------------------------------------
 
 # long time in level defined as 48 months or more
@@ -90,79 +90,92 @@ minimum_group_size <- 30
 # scipen = 999 stop scientific notation for salary data
 options(dplyr.summarise.inform = FALSE, na.action = "na.exclude", scipen = 999)
 
-# define workbook path ------------------------------------------------
 
-workbook_path <- here::here(
-  "data",
-  "Case Dataset Aerodyne.xlsx")
+# analysis functions ----------------------------------------------------
 
-# if file not found print an error to user
-if (!file.exists(workbook_path)) {
-  stop("File not found: ", workbook_path)}
+# load and check data 
+load_workbook_data <- function() {
+  # define workbook path 
+  
+  workbook_path <- here::here("data", "Case Dataset Aerodyne.xlsx")
+  
+  # if file not found print an error to user
+  if (!file.exists(workbook_path)) {
+    stop("File not found: ", workbook_path)}
+  
+  # check workbook tabs and read content 
+  required_sheets <- c(
+    "README",
+    "Data_Dictionary",
+    "Employees",
+    "Job_Architecture",
+    "Listening_Survey")
+  
+  available_sheets <- readxl::excel_sheets(workbook_path)
+  
+  missing_sheets <- setdiff(required_sheets, available_sheets)
+  
+  # if file missing tabs, print message to user
+  if (length(missing_sheets) > 0) {
+    stop("Missing workbook tabs: ", 
+         paste(missing_sheets, collapse = ", "))} 
+  else {cat("All 5 required workbook tabs are present:\n",
+            paste(available_sheets, collapse = "\n"),"\n")}
+  
+  # read workbook content 
+  readme <- readxl::read_excel(
+    workbook_path,
+    sheet = "README")
+  
+  data_dictionary <- readxl::read_excel(
+    workbook_path,
+    sheet = "Data_Dictionary")
+  
+  employees_raw <- readxl::read_excel(
+    workbook_path,
+    sheet = "Employees")
+  
+  job_architecture_raw <- readxl::read_excel(
+    workbook_path,
+    sheet = "Job_Architecture")
+  
+  listening_survey_raw <- readxl::read_excel(
+    workbook_path,
+    sheet = "Listening_Survey")
+  
+  # check sheet sizes 
+  sheet_sizes <- data.frame(
+    sheet = required_sheets,
+    rows = c(
+      nrow(readme),
+      nrow(data_dictionary),
+      nrow(employees_raw),
+      nrow(job_architecture_raw),
+      nrow(listening_survey_raw)),
+    columns = c(
+      ncol(readme),
+      ncol(data_dictionary),
+      ncol(employees_raw),
+      ncol(job_architecture_raw),
+      ncol(listening_survey_raw)))
+  
+  # provide user with sheet sizes
+  cat("5 sheet sizes:\n")
+  print(sheet_sizes)
+  
+  # returns to user from this function 
+  list(workbook_path = workbook_path,
+       required_sheets = required_sheets,
+       available_sheets = available_sheets,
+       readme = readme,
+       data_dictionary = data_dictionary,
+       employees_raw = employees_raw,
+       job_architecture_raw = job_architecture_raw,
+       listening_survey_raw = listening_survey_raw,
+       sheet_sizes = sheet_sizes
+  )
+}
 
-# check workbook tabs and read content ---------------------------------------
-
-required_sheets <- c(
-  "README",
-  "Data_Dictionary",
-  "Employees",
-  "Job_Architecture",
-  "Listening_Survey")
-
-available_sheets <- readxl::excel_sheets(workbook_path)
-
-missing_sheets <- setdiff(
-  required_sheets,
-  available_sheets)
-
-# if file missing tabs, print message to user
-if (length(missing_sheets) > 0) {
-  stop("Missing workbook tabs: ", 
-       paste(missing_sheets, collapse = ", ")
-  )} else {
-    cat("All 5 required workbook tabs are present:\n",
-        paste(available_sheets, collapse = "\n"),"\n")}
-
-# read workbook content 
-readme <- readxl::read_excel(
-  workbook_path,
-  sheet = "README")
-
-data_dictionary <- readxl::read_excel(
-  workbook_path,
-  sheet = "Data_Dictionary")
-
-employees_raw <- readxl::read_excel(
-  workbook_path,
-  sheet = "Employees")
-
-job_architecture_raw <- readxl::read_excel(
-  workbook_path,
-  sheet = "Job_Architecture")
-
-listening_survey_raw <- readxl::read_excel(
-  workbook_path,
-  sheet = "Listening_Survey")
-
-# check sheet sizes 
-sheet_sizes <- data.frame(
-  sheet = required_sheets,
-  rows = c(
-    nrow(readme),
-    nrow(data_dictionary),
-    nrow(employees_raw),
-    nrow(job_architecture_raw),
-    nrow(listening_survey_raw)),
-  columns = c(
-    ncol(readme),
-    ncol(data_dictionary),
-    ncol(employees_raw),
-    ncol(job_architecture_raw),
-    ncol(listening_survey_raw)))
-
-# provide user with sheet sizes
-cat("5 sheet sizes:\n")
-print(sheet_sizes)
 
 # inspect imported data --------------------------------------------------
 
